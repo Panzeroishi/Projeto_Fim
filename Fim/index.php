ba8b5dbc1045c700486febd0e5a418802b4cb5d8
Panzeroishi <72875007+Panzeroishi@users.noreply.github.com>
2021-11-29 21:53:12 -0300

    Add files via upload

--- Fim/index.php
+++ Fim/index.php
@@ -1,4 +1,33 @@
     <?php
-        require_once'nav.php';
+        require_once('nav.php');
 
     ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./general.css">
+        <title>Home</title>
+    </head>
+    <body>
+        <div class="menu-index">
+
+    <div class="menu1">
+        <label for="">Titulo da Atividade</label>
+        <input type="text" name="titulo" placeholder="Titulo da Atividade">
+        <textarea name="contexto" id="" cols="30" rows="10"placeholder="Contexto da Atividade"></textarea>
+    </div>
+
+
+
+        </div>
+    </body>
+    </html>
+
+        <?php
+        require_once('footer.php')
+        
+        
+        ?>
